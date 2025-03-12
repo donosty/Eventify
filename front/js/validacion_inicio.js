59597b36e25df0d001217ff40fa41fcec8259d05
donosty <mzac0077@gmail.com>
2025-03-11 22:26:56 -0600

    Avance front y JS

---
+++ front/js/validacion_inicio.js
@@ -0,0 +1,13 @@
+const correo = document.querySelector('#correo_sesion')
+const contrasena = document.querySelector('#contrasena_sesion')
+
+function eventListeners(){
+
+    correo.addEventListener('input', validacion)
+    contrasena.addEventListener('input', validacion)
+}
+
+function validacion(e){
+
+    
+}
